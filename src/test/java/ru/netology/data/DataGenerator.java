e3ddbae14f3d522cee75530ba0541aebf548dfc8
GoldFresh <valerazmey@gmail.com>
2023-04-13 15:47:01 +0300

    UsersRegistrationTest created and tested

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,75 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private DataGenerator() {
+    }
+
+    @BeforeAll
+    static void sendRequest(RegistrationDto registrationUsers) {
+        given()
+                .spec(requestSpec)
+                .body(registrationUsers)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+    }
+
+    public static RegistrationDto getActiveUser() {
+        RegistrationDto registrationUsers = new RegistrationDto(getRandomLogin(), getRandomPassword(), "active");
+        sendRequest(registrationUsers);
+        return registrationUsers;
+    }
+
+    public static RegistrationDto getBlockedUser() {
+        RegistrationDto registrationUsers = new RegistrationDto(getRandomLogin(), getRandomPassword(), "blocked");
+        sendRequest(registrationUsers);
+        return registrationUsers;
+    }
+
+
+    public static RegistrationDto getInvalidPasswordUser(String status) {
+        sendRequest(new RegistrationDto(getRandomLogin(), getRandomPassword(), status));
+        return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+    }
+
+    public static RegistrationDto getInvalidLoginUser(String status) {
+        sendRequest(new RegistrationDto(getRandomLogin(), getRandomPassword(), status));
+        return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+    }
+}
---
+++ src/test/java/ru/netology/test/UsersRegistrationTest.java
@@ -0,0 +1,54 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataGenerator;
+import ru.netology.data.RegistrationDto;;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class UsersRegistrationTest {
+
+    @BeforeEach
+    void Setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldLoginIfActiveUserRegistered() {
+        RegistrationDto activeUser = DataGenerator.getActiveUser();
+        $("[data-test-id=login] input").setValue(activeUser.getLogin());
+        $("[data-test-id=password] input").setValue(activeUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(".App_appContainer__3jRx1").shouldHave(Condition.text("Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldGetErrorIfBlockedUser() {
+        RegistrationDto blockedUser = DataGenerator.getBlockedUser();
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("Пользователь заблокирован"));
+    }
+
+    @Test
+    void shouldGetErrorIfWrongLogin() {
+        RegistrationDto invalidLoginUser = DataGenerator.getInvalidLoginUser("active");
+        $("[data-test-id=login] input").setValue(invalidLoginUser.getLogin());
+        $("[data-test-id=password] input").setValue(invalidLoginUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldGetErrorIfWrongPassword() {
+        RegistrationDto invalidPassword = DataGenerator.getInvalidPasswordUser("active");
+        $("[data-test-id=login] input").setValue(invalidPassword.getLogin());
+        $("[data-test-id=password] input").setValue(invalidPassword.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("Неверно указан логин или пароль"));
+    }
+}
